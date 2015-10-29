4b12dc22f0a04f2ed89853590a882586941faa1c
Zhikharev <Vvoopprroos@yandex.ru>
2015-10-30 01:26:49 +0300

    add files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "sdt.h"
+
+int main()
+{
+   int x,y;
+   cout <<"Vvedite chislo x:\n";
+   cin>> x;
+   cout <<"Vvedite chislo y:\n";
+   cin>> y;
+   sum=x+y;
+   cout <<"Sum: ";
+   cout <<sum;
+}
